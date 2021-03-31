13433d03b8486ed5ebb9d7ee3d5571f1f6c8f28a
anle9650 <andy.le@ucdenver.edu>
2021-03-30 20:54:03 -0600

    update saveUser

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -4,6 +4,17 @@ const User = require("../models/user");
 
 exports.getSignupPage = (req, res) => {
     res.render("signup", {
+        firstNameError: false,
+        lastNameError: false,
+        userNameError: false,
+        emailError: false,
+        DOBError: false,
+        cityError: false,
+        stateError: false,
+        passwordError: false,
+        confirmPasswordError: false,
+        securityQuestionError: false,
+        answerError: false,
         successMessage: "",
         errorMessage: ""
     });
@@ -16,19 +27,38 @@ exports.getLoginPage = (req, res) => {
 };
 
 exports.saveUser = (req, res) => {
-    // Displays single error message if fields are not all entered, or if passwords don't match.
-    // Still need to figure out how to change background color for each incorrect input.
-    if (!req.body.txtFirstName || !req.body.txtLastName || !req.body.txtUserName || !req.body.txtEmail || !req.body.gender || !req.body.txtDOB || !req.body.txtCity || !req.body.txtState ||
-        !req.body.txtPassword || !req.body.ddQuestions || !req.body.txtAnswer) {
+    if (!req.body.txtFirstName || !req.body.txtLastName || !req.body.txtUserName || !req.body.txtEmail || !req.body.txtDOB || !req.body.txtCity || !req.body.txtState ||
+        !req.body.txtPassword || !req.body.txtConfirmPassword || !req.body.ddQuestions || !req.body.txtAnswer) {
         res.render("signup", {
+            firstNameError: !req.body.txtFirstName,
+            lastNameError: !req.body.txtLastName,
+            userNameError: !req.body.txtUserName,
+            emailError: !req.body.txtEmail,
+            DOBError: !req.body.txtDOB,
+            cityError: !req.body.txtCity,
+            stateError: !req.body.txtState,
+            passwordError: !req.body.txtPassword,
+            confirmPasswordError: !req.body.txtConfirmPassword,
+            securityQuestionError: !req.body.ddQuestions,
+            answerError: !req.body.txtAnswer,
             successMessage: "",
-            errorMessage: "All fields must be entered."
+            errorMessage: "Please complete the highlighted fields."
         });
         return;
     }
-
     else if (req.body.txtPassword != req.body.txtConfirmPassword) {
         res.render("signup", {
+            firstNameError: false,
+            lastNameError: false,
+            userNameError: false,
+            emailError: false,
+            DOBError: false,
+            cityError: false,
+            stateError: false,
+            passwordError: true,
+            confirmPasswordError: true,
+            securityQuestionError: false,
+            answerError: false,
             successMessage: "",
             errorMessage: "Passwords must match."
         });
@@ -53,6 +83,17 @@ exports.saveUser = (req, res) => {
     newUser.save()
         .then(result => {
             res.render("signup", {
+                firstNameError: false,
+                lastNameError: false,
+                userNameError: false,
+                emailError: false,
+                DOBError: false,
+                cityError: false,
+                stateError: false,
+                passwordError: false,
+                confirmPasswordError: false,
+                securityQuestionError: false,
+                answerError: false,
                 successMessage: "Your account has been successfully created!",
                 errorMessage: ""
             })
